f834e4f96b021e7891fedc6194d45adf930431e8
Aleksandr <dentranscom@gmail.com>
2022-10-28 09:46:51 +0300

    fix data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,9 @@
-import Dragon from "../assets/imgs/Dragon.jpg";
+import To_Do_List from "../assets/imgs/To_Do_List.jpg";
 import Employees from "../assets/imgs/Employees.jpg";
 import Marvel from "../assets/imgs/Marvel.jpg";
 import Clothing from "../assets/imgs/Clothing.jpg";
 
 export const projectsData = [
-  {
-    img: Dragon,
-    name: "Dragon_Game",
-    github: "https://github.com/AleksandrMyagkiy",
-    link: "https://aleksandrmyagkiy.github.io/DragonGame/",
-  },
-  {
-    img: Employees,
-    name: "Employees",
-    github: "https://github.com/AleksandrMyagkiy",
-    link: "https://employees123.herokuapp.com/",
-  },
   {
     img: Clothing,
     name: "E-commerce",
@@ -27,5 +15,17 @@ export const projectsData = [
     name: "Marvel",
     github: "https://github.com/AleksandrMyagkiy/MARVEL",
     link: "https://lighthearted-melomakarona-bd30de.netlify.app/",
+  },
+  {
+    img: To_Do_List,
+    name: "To Do List ",
+    github: "https://github.com/AleksandrMyagkiy/To-Do-List",
+    link: "https://monumental-gumdrop-8ab571.netlify.app/",
+  },
+  {
+    img: Employees,
+    name: "Employees",
+    github: "https://github.com/AleksandrMyagkiy",
+    link: "https://employees123.herokuapp.com/",
   },
 ];
